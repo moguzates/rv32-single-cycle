9948a1e4449e94e086d875881433343b33750412
moguzates <mustafaoguzates@gmail.com>
2025-12-25 21:56:34 +0300

    Update opcode decoding

--- src/core_model.sv
+++ src/core_model.sv
@@ -11,8 +11,9 @@ module core_model
 
     //Memory
     parameter int MEM_SIZE = 1024;
-    logic [31:0] imem [MEM_SIZE-1:0];
-    logic [31:0] dmem [MEM_SIZE-1:0];
+    logic [31:0]       imem [MEM_SIZE-1:0];
+    logic [31:0]       dmem [MEM_SIZE-1:0];
+    logic [XLEN-1 : 0] rf   [31:0]; //register file
 
     //pc + instr
     logic [XLEN-1 : 0] pc_d;
@@ -20,6 +21,12 @@ module core_model
     logic [XLEN-1 : 0] jump_pc_d;
     logic              jump_pc_valid_d;
     logic [XLEN-1 : 0] instr_d;
+
+    logic [XLEN-1 : 0] rs1_data;
+    logic [XLEN-1 : 0] rs2_data;
+    logic [XLEN-1 : 0] imm_data;
+    logic [4:0]        shamt;
+    logic [XLEN-1 : 0] rd_data; //register destination
 
     always_ff @(posedge clk_i) begin : pc_change_block
         if(~rstn_i) begin
@@ -38,80 +45,97 @@ module core_model
         end
         instr_d = imem[pc_d[$clog2(MEM_SIZE)-1:0]];
     end
-/*
+
     always_comb begin : decode
         case(instr_d[6:0])
-            OpcodeLui :
-            OpcodeAuipc:
-            OpcodeJal : 
-            OpcodeJalr :
-            OpcodeBranch :
+            OpcodeLui :   imm_data = {instr_d[31:12] , 12'b0};
+            OpcodeAuipc : imm_data = {instr_d[31:12] , 12'b0};
+            OpcodeJal :   imm_data = {{12'({instr_d[31]})}, instr_d[19:12], instr_d[20], instr_d[30:21], 1'b0};             
+            OpcodeJalr : begin
+                if(instr_d[14:12] == F3_JALR) begin
+                    rs1_data = rf[instr_d[19:15]]; 
+                    imm_data = {{21'({instr_d[31]})}, instr_d[30:20]};  
+                end           
+            end
+            OpcodeBranch : begin
+                if (instr_d[14:12] inside {F3_BEQ, F3_BNE, F3_BLT, F3_BGE, F3_BLTU, F3_BGEU}) begin
+                    rs1_data = rf[instr_d[19:15]]; 
+                    rs2_data = rf[instr_d[24:20]];
+                    imm_data = {{18'({instr_d[31]})}, instr_d[31], instr_d[7], instr_d[10:5], instr_d[11:7], 1'b0};
+                end
+            end
+            OpcodeLoad : begin
+                rs1_data = rf[instr_d[19:15]];
+                imm_data = {{20'({instr_d[31]})}, instr_d[31:20]};
+            end
+            OpcodeStore : begin
+                rs1_data = rf[instr_d[19:15]]; 
+                rs2_data = rf[instr_d[24:20]];
+                imm_data = {{20'({instr_d[31]})}, instr_d[31:25], instr_d[11:7]};
+            end
+            OpcodeOpImm : begin
                 case(instr_d[14:12])
-                    F3_BEQ  :
-                    F3_BNE  :
-                    F3_BLT  :
-                    F3_BGE  :
-                    F3_BLTU :
-                    F3_BGEU :
-                endcase
-            OpcodeLoad :
-                case(instr_d[14:12])
-                    F3_LB  :
-                    F3_LH  :
-                    F3_LW  :
-                    F3_LBU :
-                    F3_LHU :
-                endcase
-            OpcodeStore :
-                case(instr_d[14:12])
-                    F3_SB :
-                    F3_SH :
-                    F3_SW :
-                endcase
-            OpcodeOpImm :
-                case(instr_d[14:12])
-                    F3_ADDI  : 
-                    F3_SLTI  : 
-                    F3_SLTIU : 
-                    F3_XORI  : 
-                    F3_ORI   : 
-                    F3_ANDI  : 
-                    F3_SLLI  :
+                    F3_ADDI, F3_SLTI, F3_SLTIU, F3_XORI, F3_ORI, F3_ANDI : begin
+                        rs1_data = rf[instr_d[19:15]];
+                        imm_data = {{20'({instr_d[31]})}, instr_d[31:20]};
+                    end
+                    F3_SLLI :
                         if(instr_d[31:25] == F7_SLLI) begin
+                            shamt = instr_d[24:20];
+                            rs1_data = rf[instr_d[19:15]];
                         end
-                    F3_SRLI  : 
-                        if(instr_d[31:25] == F7_SRLI) begin
-                        end
-                    F3_SRAI  : 
-                        if(instr_d[31:25] == F7_SRAI) begin
+                    F3_SRLI : 
+                        if(instr_d[31:25] inside {F7_SRLI, F7_SRAI}) begin
+                            shamt = instr_d[24:20];
+                            rs1_data = rf[instr_d[19:15]];
                         end
                 endcase
+            end
             OpcodeOp :
                 case(instr_d[14:12])
-                    F3_ADD  :
-                        if(instr_d[31:25] == F7_ADD)
-                    F3_SUB  :
-                        if(instr_d[31:25] == F7_SUB)
+                    F3_ADD :
+                        if(instr_d[31:25] inside {F7_ADD, F7_SUB}) begin
+                            rs1_data = rf[instr_d[19:15]];
+                            rs2_data = rf[instr_d[24:20]];        
+                        end 
                     F3_SLL  :
-                        if(instr_d[31:25] == F7_SLL)
+                        if(instr_d[31:25] == F7_SLL) begin
+                            rs1_data = rf[instr_d[19:15]];
+                            rs2_data = rf[instr_d[24:20]];        
+                        end
                     F3_SLT  :
-                        if(instr_d[31:25] == F7_SLT)
+                        if(instr_d[31:25] == F7_SLT) begin
+                            rs1_data = rf[instr_d[19:15]];
+                            rs2_data = rf[instr_d[24:20]];        
+                        end
                     F3_SLTU :
-                        if(instr_d[31:25] == F7_SLTU)
+                        if(instr_d[31:25] == F7_SLTU) begin
+                            rs1_data = rf[instr_d[19:15]];
+                            rs2_data = rf[instr_d[24:20]];        
+                        end
                     F3_XOR  :
-                        if(instr_d[31:25] == F7_XOR)
+                        if(instr_d[31:25] == F7_XOR) begin
+                            rs1_data = rf[instr_d[19:15]];
+                            rs2_data = rf[instr_d[24:20]];        
+                        end
                     F3_SRL  :
-                        if(instr_d[31:25] == F7_SRL)
-                    F3_SRA  :
-                        if(instr_d[31:25] == F7_SRA)
+                        if(instr_d[31:25] inside {F7_SRL, F7_SRA}) begin
+                            rs1_data = rf[instr_d[19:15]];
+                            rs2_data = rf[instr_d[24:20]];        
+                        end
                     F3_OR   :
-                        if(instr_d[31:25] == )F7_OR
+                        if(instr_d[31:25] == F7_OR) begin
+                            rs1_data =rf[instr_d[19:15]];
+                            rs2_data =rf[instr_d[24:20]];        
+                        end
                     F3_AND  :
-                        if(instr_d[31:25] == F7_AND)
+                        if(instr_d[31:25] == F7_AND) begin
+                            rs1_data =rf[instr_d[19:15]];
+                            rs2_data =rf[instr_d[24:20]];        
+                        end
                 endcase
-
+                default : ;
         endcase
     end
-*/
 
 endmodule
